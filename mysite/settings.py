f137504422beacfece1833349b359493f229732d
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2023-04-25 20:38:05 +0200

    Ajustes de la database

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': "aH43rk4OFdtBYM1aZgOa",
+        'HOST': "containers-us-west-108.railway.app",
+        'PORT': "7833",
     }
 }
 
