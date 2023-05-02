22aa2612c9f19a48da5353f96a7e7cebd959ba8b
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2023-05-02 20:26:34 +0200

    Reparando errores con mas errores

--- cataleg/views.py
+++ cataleg/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+producto = {
+            'name': 'silla',
+            'descripcion': 'silla de plastico para jardin super resistente',
+            'precio': '20€',
+        }
+
+def index(request):
+
+    return render(request,'index.html', {'Nombre': producto['name'],
+                                         'Descripcion': producto['descripcion'],
+                                         'Precio': producto['precio']
+                                        })
+
