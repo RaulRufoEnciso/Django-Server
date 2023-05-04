afb9e25ca3e29b190f27dc0b933878322bb5b71e
sebastian11223 <mcasco@jaumebalmes.net>
2023-05-04 21:10:12 +0200

    hecho modelo y funcion de estado en view

--- comandes/models.py
+++ comandes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Comanda(models.Model):
+    ESTAT_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('pagado', 'Pagado'),
+    )
+
+# usamos el método choices para poder escoger el tipo de estado del oedido
+    estat = models.CharField(max_length=20, choices=ESTAT_CHOICES, default='pendiente')
+
+
--- comandes/views.py
+++ comandes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -15,3 +17,12 @@ def getRuta(request):
         {'HOLA2':'ADIOS2'},
     ]
     return Response(list)
+
+
+
+def actualizar_estado_pedido(request, comanda_id):
+    comanda = get_object_or_404(comanda, pk=comanda_id)
+    # falta definir los atributos del objeto comanda en el modelo de la app comandes
+    comanda.estat = 'pagado'
+    comanda.save()
+    return JsonResponse({'success': True})
